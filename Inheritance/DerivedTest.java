86adea8e7d5e7cb72ad6bb40b200dbea325d142e
dat-adi <dat.adithya@gmail.com>
2020-10-14 21:33:48 +0530

    updated indentation in Inheritance

--- Inheritance/DerivedTest.java
+++ Inheritance/DerivedTest.java
@@ -1,21 +1,16 @@
-class Base
-{
+class Base {
 	int a = 40;
 	int b = 30;
 }
-class Derived extends Base
-{
+class Derived extends Base {
 	int a = 400;
 	int b = 300;
-	void add(int a, int b)
-	{
-	//code
+	void add(int a, int b) {
+        //code
 	}
 }
-class DerivedTest
-{
-	public static void main(String[] args)
-	{
+class DerivedTest {
+	public static void main(String[] args) {
 		Derived d = new Derived();
 		d.add(4000,3000);
 	}
